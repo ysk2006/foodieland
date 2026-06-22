14d80178a53ab492e57e5488c3c2f4907af984f5
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-06-22 17:32:25 +0300

    create(context):  info for banner

---
+++ src/context/bannerTypes.js
@@ -0,0 +1,21 @@
+import Recipe from "/public/png/recipe.png";
+import Tasty from "/public/png/tasty.png";
+import Faster from "/public/png/faster.png";
+
+export const BannerTypes = [
+  {
+    id: 0,
+    name: "recipe",
+    icon: Recipe,
+  },
+  {
+    id: 1,
+    name: "tasty",
+    icon: Tasty,
+  },
+  {
+    id: 2,
+    name: "faster",
+    icon: Faster,
+  },
+];
