362c3796ecc8cf7349e304f02453aed73a54b692
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-06-22 17:32:13 +0300

    create(context): types info for banner

---
+++ src/context/bannerTags.js
@@ -0,0 +1,29 @@
+import { Clock, Utensils, Fish, FastForward, Laugh } from "lucide-react";
+
+export const bannerTags = [
+  {
+    id: 0,
+    name: "Minutes",
+    icon: Clock,
+  },
+  {
+    id: 1,
+    name: "Chicken",
+    icon: Utensils,
+  },
+  {
+    id: 2,
+    name: "Fish",
+    icon: Fish,
+  },
+  {
+    id: 3,
+    name: "Fast",
+    icon: FastForward,
+  },
+  {
+    id: 4,
+    name: "Very tasty",
+    icon: Laugh,
+  },
+];
