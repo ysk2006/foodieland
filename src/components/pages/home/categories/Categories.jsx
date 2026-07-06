6f390ed8323337e9925e3bce3e5c8f9ecc1c184f
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-07-06 14:14:20 +0300

    create ctaegories section component

--- src/components/pages/home/categories/Categories.jsx
+++ src/components/pages/home/categories/Categories.jsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router";
+import CategoryCard from "@/components/ui/cards/category-card/CategoryCard";
+
+import { categoriesMockData } from "@/data/mockCategories";
+
 import styles from "./Categories.module.scss";
 
-function Categories() {}
+function Categories() {
+    return (
+        <section className={styles.category}>
+            <div className={styles.category__header}>
+                <h2>Categories</h2>
+                <Link className={styles.category__link}>
+                    View All Categories
+                </Link>
+            </div>
+            <div className={styles.category__body}>
+                {categoriesMockData.map((caty) => (
+                    <CategoryCard
+                        id={caty.id}
+                        name={caty.name}
+                        image={caty.image}
+                    />
+                ))}
+            </div>
+        </section>
+    );
+}
 
 export default Categories;
