e9df611cd6329a1a093b8750d5b4c8b50e0e0d0e
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-07-06 14:13:43 +0300

    -

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router";
 import Header from "@/components/layout/header/Header";
 
 function RootLayout() {
-  return (
-    <div className="app">
-      <Header />
-      <main className="container">
-        <Outlet />
-      </main>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Header />
+            <main className="container">
+                <Outlet />
+            </main>
+        </div>
+    );
 }
 
 export default RootLayout;
