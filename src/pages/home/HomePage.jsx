ba0ebc3eb1ee441a03c85984d77ca51d70aad34e
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-07-06 14:13:51 +0300

    add new page

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,23 +5,25 @@ import { PATHS } from "@/app/router/paths";
 import { usePageSEO } from "@/hooks/usePageSEO";
 
 import Hero from "@/components/pages/home/hero/Hero";
+import Categories from "@/components/pages/home/categories/Categories";
 
 function HomePage() {
-  const seo = usePageSEO({
-    title: "Главная",
-    description: "Откройте для себя мир вкусной еды с Foodieland",
-    path: PATHS.HOME,
-    image: "/og-image.jpg",
-  });
+    const seo = usePageSEO({
+        title: "Главная",
+        description: "Откройте для себя мир вкусной еды с Foodieland",
+        path: PATHS.HOME,
+        image: "/og-image.jpg",
+    });
 
-  return (
-    <>
-      <SEO {...seo} />
-      <div className="home-page">
-        <Hero />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <SEO {...seo} />
+            <div className="home-page">
+                <Hero />
+                <Categories />
+            </div>
+        </>
+    );
 }
 
 export default HomePage;
