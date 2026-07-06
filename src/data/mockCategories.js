9f2320c170906c833da8c47b55948d0e58f4febd
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-07-06 14:13:28 +0300

    -

---
+++ src/data/mockCategories.js
@@ -0,0 +1,39 @@
+import Breakfast from "/public/png/breakfast.png";
+import Vegan from "/public/png/vegan.png";
+import Meat from "/public/png/meat.png";
+import Dessert from "/public/png/dessert.png";
+import Lunch from "/public/png/lunch.png";
+import Chocolate from "/public/png/chocolate.png";
+
+export const categoriesMockData = [
+    {
+        id: 0,
+        name: "Breakfast",
+        image: Breakfast,
+    },
+    {
+        id: 1,
+        name: "Vegan",
+        image: Vegan,
+    },
+    {
+        id: 2,
+        name: "Meat",
+        image: Meat,
+    },
+    {
+        id: 3,
+        name: "Dessert",
+        image: Dessert,
+    },
+    {
+        id: 4,
+        name: "Lunch",
+        image: Lunch,
+    },
+    {
+        id: 5,
+        name: "Chocolate",
+        image: Chocolate,
+    },
+];
