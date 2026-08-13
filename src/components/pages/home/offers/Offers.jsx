40efe534bf6d966221b73fe3a5afbfc539105397
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-08-13 14:33:12 +0300

    create(component): special offers recipes section
    Add the Try this delicious recipe section that reuses RecipeCard for featured dishes.

---
+++ src/components/pages/home/offers/Offers.jsx
@@ -0,0 +1,38 @@
+import { mockRecipeCards } from "@/data/mockRecipeCards";
+import RecipeCard from "@/components/ui/cards/recipe-card/RecipeCard";
+
+import styles from "./Offers.module.scss";
+
+function Offers() {
+    return (
+        <section className={styles.offers}>
+            <div className={styles.offers__header}>
+                <h2>
+                    Try this delicious recipe
+                    <br />
+                    to make your day
+                </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqut
+                    enim ad minim
+                </p>
+            </div>
+            <div className={styles.offers__grid}>
+                {[...mockRecipeCards].reverse().map((recipe) => (
+                    <RecipeCard
+                        key={recipe.id}
+                        id={recipe.id}
+                        title={recipe.title}
+                        time={recipe.time}
+                        image={recipe.image}
+                        category={recipe.category}
+                        compact
+                    />
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default Offers;
