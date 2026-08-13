7f9aee09378390cc31f1c6fddfe1b98b367f746a
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2026-08-13 14:32:36 +0300

    create(component): home recipes section
    Render a grid of recipe cards on the home page with the Simple and tasty recipes heading.

---
+++ src/components/pages/home/recipes/Recipes.jsx
@@ -0,0 +1,37 @@
+import { mockRecipeCards } from "@/data/mockRecipeCards";
+import RecipeCard from "@/components/ui/cards/recipe-card/RecipeCard";
+
+import styles from "./Recipes.module.scss";
+
+function Recipes() {
+    return (
+        <section className={styles.recipes__section}>
+            <div className={styles.recipes__text}>
+                <h2 className={styles["recipes__text--title"]}>
+                    Simple and tasty recipes
+                </h2>
+                <div className={styles["recipes__text--description"]}>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetuipisicing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqut enim ad minim{" "}
+                    </p>
+                </div>
+            </div>
+            <div className={styles.recipes__wrapper}>
+                {mockRecipeCards.map((recipe) => (
+                    <RecipeCard
+                        key={recipe.id}
+                        id={recipe.id}
+                        title={recipe.title}
+                        time={recipe.time}
+                        image={recipe.image}
+                        category={recipe.category}
+                    />
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default Recipes;
